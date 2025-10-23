67d37a985db5fdd7838770f41badad9c7e28d6b5
Volodymyr Oprysk <volodymyr.oprysk_ext@50hertz.com>
2025-10-23 11:05:21 +0300

    Extebnded HyDEX Features list (text on home page)

--- pages/home.py
+++ pages/home.py
@@ -7,6 +7,7 @@ st.markdown(
     HyDEX is a  data visualization platform built with Python and Streamlit, an open-source app framework tailored for Machine Learning and Data Science projects.
 
     ### HyDEX Features:
+    - **X-Ray Diffraction (XRD) Analysis**: Visualize and compare XRD diffraction patterns with multi-file overlay support.
     - **Charge Discharge Visualization**: Gain insights into charge and discharge curves with intuitive visualizations.
     - **Cyclic Voltammetry Analysis (CVA)**: Explore cyclic voltammetry graphics to analyze electrochemical processes.
     - **Stay Tuned for More**: We're constantly adding new features to enhance our data visualization abilities.
